36123507514b217eb326426df69156acc020740d
tanselbaran <tanselbaranyasar@gmail.com>
2018-07-16 18:00:16 +0200

    Modified the id attribute of the 32_channel_ecog according to the new id format

--- probe_files/32_channel_ecog.py
+++ probe_files/32_channel_ecog.py
@@ -6,10 +6,11 @@ probe_info['nr_of_electrodes_per_group'] = 8
 probe_info['numTrodes'] = 32
 
 ###Channel mapping###
-id = np.zeros((probe_info['nr_of_electrodes_per_group'], probe_info['nr_of_groups']))
+id = {}
 electrode = 0
 for group in range(probe_info['nr_of_groups']):
+    id[group] = []
     for channel in range(probe_info['nr_of_electrodes_per_group']):
-        id[channel][group] = electrode
+        id[group].append(electrode)
         electrode = electrode + 1
 probe_info['id'] = id
