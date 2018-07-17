5683ab8a7dfee53a5af139cfa3138499f442de4b
tanselbaran <tanselbaranyasar@gmail.com>
2018-07-17 12:09:37 +0200

    Fixed issues with tabs and spacings

--- spikeSortingUtils/custom_spike_sorting_utils.py
+++ spikeSortingUtils/custom_spike_sorting_utils.py
@@ -34,7 +34,7 @@ def read_location(location, group):
     reading_index = 0
     while reading_index < len(time_session):
         chunk_time = time[reading_index:reading_index + chunk_size] #Time interval of the chunk in s
-        chunk = np.fromfile(data_file, filetype = 'int16', len(channels)*chunk_size) #Data for the chunk in compressed format
+        chunk = np.fromfile(data_file, 'int16', len(channels)*chunk_size) #Data for the chunk in compressed format
         reshaped_chunk = np.reshape(chunk, (chunk_size, len(channels))) #Reshaping the chunk into a 2d array
         reshaped_chunk = np.transpose(reshaped_chunk) #Transposing the chunk to get the N_electrodes x N_samples format in the array
         filtered_chunk = experiment.bandfilt(reshaped_chunk) #Bandpass filtering the chunkchannels
@@ -52,53 +52,52 @@ def read_location(location, group):
     return location_output
 
 def extract_waveforms(data, time, location):
-	"""
-	This function extracts waveforms from multiple channel data based on the
-	given threshold coefficient. A relative threshold is calculated by
-	multiplying the rms of the recording with the given threshold coefficient.
-	For each time step the function scans through all the electrodes to find an
-	event of threshold crossing. If so, the waveforms in all channels surrounding
-	that time step are recorded.
+    """
+    This function extracts waveforms from multiple channel data based on the
+    given threshold coefficient. A relative threshold is calculated by
+    multiplying the rms of the recording with the given threshold coefficient.
+    For each time step the function scans through all the electrodes to find an
+    event of threshold crossing. If so, the waveforms in all channels surrounding
+    that time step are recorded.
 
-	Inputs:
-		data: Numpy array containing the bandpass filtered data in the form of
-			(N_electrodes x N_time steps)
-		params: Dictionary containing the recording parameters. The following
-			entries must be present:
+    Inputs:
+        data: Numpy array containing the bandpass filtered data in the form of
+            (N_electrodes x N_time steps)
+        params: Dictionary containing the recording parameters. The following
+            entries must be present:
 
-			sample_rate: Sampling rate in Hz
-			time: Time array of the recording (numpy array)
-			pre: Extent of the spike time window prior to peak (in ms)
-			post: Extent of the spike time window post the peak (in ms)
-			flat_map_array: Flattened numpy array containing the spatial
-				information of the electrodes (contains only one element
-				for single channel recordings)
-			threshold_coeff: Coefficient used for calculating the threshold
-				value per channel
+            sample_rate: Sampling rate in Hz
+            time: Time array of the recording (numpy array)
+            pre: Extent of the spike time window prior to peak (in ms)
+            post: Extent of the spike time window post the peak (in ms)
+            flat_map_array: Flattened numpy array containing the spatial
+                information of the electrodes (contains only one element
+                for single channel recordings)
+            threshold_coeff: Coefficient used for calculating the threshold
+                value per channel
 
-	Outputs:
-		waveforms: Numpy array containing the extracted waveforms from all
-			channels (N_events x N_electrodes x N_timesteps_in_spike_time_range)
-		peak_times: numpy array containing the times of the events (in s)
-			(1xN_events)
-	"""
+    Outputs:
+        waveforms: Numpy array containing the extracted waveforms from all
+            channels (N_events x N_electrodes x N_timesteps_in_spike_time_range)
+        peak_times: numpy array containing the times of the events (in s)
+            (1xN_events)
+    """
 
     experiment = location.experiment
-	threshold = noise * experiment.threshold_coeff
+    threshold = noise * experiment.threshold_coeff
+    waveforms = []
+    peak_times = []
+    found = False
+    for i in range(experiment.spike_samples_before, len(data[0]) - experiment.spike_samples_after):
+        for trode in range(len(data)):
+            if (abs(data[trode,i] > threshold[trode])) and (abs(data[trode,i]) > abs(data[trode,i-1])) and (abs(data[trode,i]) > abs(data[trode,i+1])):
+                found = True
+                break
+        waveform = data[:,(i - experiment.spike_samples_before):(i + experiment.spike_samples_after)]
+        waveforms.append(waveform)
+        peak_times.append(time[i])
+        found = False
 
-	waveforms = []
-	peak_times = []
-	found = False
-	for i in range(experiment.spike_samples_before, len(data[0]) - experiment.spike_samples_after):
-		for trode in range(len(data)):
-			if (abs(data[trode,i) > threshold[trode]) and (abs(data[trode,i]) > abs(data[trode,i-1])) and (abs(data[trode,i]) > abs(data[trode,i+1])):
-				found = True
-				break
-		waveform = data[:,(i - experiment.spike_samples_before):(i + experiment.spike_samples_after)]
-		waveforms.append(waveform)
-		peak_times.append(time[i])
-		found = False
-
-	waveforms = np.asarray(waveforms)
-	peak_times = np.asarray(peak_times)
-	return waveforms, peak_times
+    waveforms = np.asarray(waveforms)
+    peak_times = np.asarray(peak_times)
+    return waveforms, peak_times
